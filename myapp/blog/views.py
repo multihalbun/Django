34f4ec26f5cedb0434de11d4e8e6692e5ee95157
Jinchan Kim <wlscks7@naver.com>
2023-06-21 16:03:19 +0900

    230621 django

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import ListView, CreateView, DetailView
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 # def index(request):
@@ -14,4 +17,50 @@ from django.views import View
 class Index(View):
     def get(self, request):
         # return HttpResponse('Index page GET class')
-        return render(request, 'blog/board.html')
+
+        # 데이터베이스에 접근해서 값을 가져와야 합니다.
+        # 게시판에 글들을 보여줘야하기 때문에 데이터베이스에서 "값 조회"
+        # MyModel.objects.all()
+        post_objs = Post.objects.all()
+        # context = 데이터베이스에서 가져온 값
+        context = {
+            "posts": post_objs
+        }
+        # print(post_objs)
+        return render(request, 'blog/board.html', context)
+    
+
+# write
+# post - form
+# 글 작성 화면
+def write(request):
+    if request.method == 'POST':
+        # form 확인
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:list')
+        
+    form = PostForm()
+    return render(request, 'blog/write.html', { 'form': form })
+
+# Django 자체의 클래스 뷰 기능도 강력, 편리
+# model, template_name, context_object_name,
+# paginate_by, form_class, form_valid(), get_queryset()
+# django.views.generic -> ListView
+class List(ListView):
+    model = Post # 모델
+    template_name = 'blog/post_list.html' # 템플릿
+    context_object_name = 'posts' # 변수 값의 이름
+
+
+class Write(CreateView):
+    model = Post # 모델
+    form_class = PostForm # 폼
+    success_url = reverse_lazy('blog:list') # 성공시 보내줄 url
+
+
+class Detail(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
