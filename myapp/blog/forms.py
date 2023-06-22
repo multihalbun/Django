24bf8dc78094119e889fb6c37ab06cdcbc51ea37
Jinchan Kim <wlscks7@naver.com>
2023-06-22 16:56:40 +0900

    230622 django

--- myapp/blog/forms.py
+++ myapp/blog/forms.py
@@ -1,6 +1,6 @@
 # blog/forms.py
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 # Form: html에 있는 form 태그
 # Model Form: model을 사용하는 form
@@ -13,3 +13,10 @@ class PostForm(forms.ModelForm):
         # widget = {
         #     'content': forms.widgets.Textarea
         # }
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['content']
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic import ListView, CreateView, DetailView
-from .models import Post
-from .forms import PostForm
-from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy, reverse
 
 # Create your views here.
 # def index(request):
@@ -64,3 +64,56 @@ class Detail(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+
+class Update(UpdateView):
+    model = Post
+    template_name ='blog/post_edit.html'
+    fields = ['title', 'content', 'writer']
+    # success_url = reverse_lazy('blog:list')
+
+    # initial 기능 -> form에 값을 미리 넣어주기 위해서
+    def get_initial(self):
+        initial = super().get_initial() # UpdateView(generic view)에서 제공하는 initial(딕셔너리)
+        post = self.get_object() # pk 기반으로 객체를 가져옴
+        initial['title'] = post.title
+        initial['content'] = post.content
+        initial['writer'] = post.writer
+        return initial
+    
+    def get_success_url(self): # get_absolute_url
+        post = self.get_object() # pk 기반으로 현재 객체 가져오기
+        return reverse('blog:detail', kwargs={ 'pk': post.pk })
+
+
+class Delete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:list')
+
+
+class DetailView(View):
+    def get(self, request, post_id):
+        # list -> object 상세 페이지 -> 상세 페이지 하나의 내용
+        # pk 값을 왔다갔다, 하나의 인자
+
+        # 데이터베이스 방문
+        # 해당 글
+        post = Post.objects.get(pk=post_id)
+        # 댓글
+        
+
+
+### Comment
+class CommentWrite(View):
+    # def get(self, request):
+    #     pass
+    def post(self, request, post_id):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            # 사용자에게 댓글 내용을 받아왐
+            content = form.cleaned_data['content']
+            # 해당 아이디에 해당하는 글 불러옴
+            post = Post.objects.get(pk=post_id)
+            # 댓글 객체 생성
+            comment = Comment.objects.create(post=post, content=content)
+            return redirect('blog:detail', pk=post_id)
