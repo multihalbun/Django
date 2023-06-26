8fe660ae4bac3ead01e489527b11efeb336201c1
Jinchan Kim <wlscks7@naver.com>
2023-06-26 16:37:21 +0900

    django 230626

---
+++ myapp/user/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import User
+
+
+class RegisterForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'password']
+
+
+class LoginForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
